911dc1a307088cf9390d99cedb6e70741c121ace
Kurchev-Nuri <nuriabaza90@gmail.com>
2019-09-22 16:30:37 +0300

    initialization

---
+++ Russian.Post/Russian.Post.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Russian.Post.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
